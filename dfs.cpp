58ab2a09b0472953675b32f8a45b3d5a15ccd3f3
NutFactory <thatshaley@hotmail.com>
2018-11-01 10:29:59 -0400

    Lab 6 finished :))

--- dfs.cpp
+++ dfs.cpp
@@ -47,14 +47,15 @@ Edges getEdges( )
 
 }
 
-void dfs(unsigned v, Graph& g, unsigned& count)
+void dfs(unsigned v, Graph& g, unsigned& count, unsigned& comp)
 {
 	count++;
 	g.V[v].visited = count;
+	g.V[v].component = comp;
 	
   	for(auto w: g.E[v])//iterate over neighbours of node v
   	  if ( g.V[w].visited == 0 )
-		dfs(w, g, count);
+		dfs(w, g, count, comp);
 	
 }
 
@@ -65,12 +66,14 @@ void mainDFS(const Edges& E)
   G.V.resize(E.size());
   G.E = E; //Hopefully the compiler optimizes this 
   unsigned counter = 0;
+  unsigned comp = 0;
   
   for(unsigned i = 0; i < G.V.size(); i++)//iterate over nodes
     {
 	   if(G.V[i].visited == 0)
 	     {		     
-	       dfs(i, G, counter);		
+			comp = comp + 1;
+	       dfs(i, G, counter, comp);		
 	      }  
      }  	
       	
@@ -78,7 +81,7 @@ void mainDFS(const Edges& E)
    
   for(unsigned i =0; i < G.V.size();i++)
   {
-    cerr << i<<" visited "<< G.V[i].visited << endl;
+    cerr << i<<" visited "<< G.V[i].visited << " Component : " << G.V[i].component << endl;
    }
     
 }
